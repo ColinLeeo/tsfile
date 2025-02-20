bc359b641eada04801740b5a2ca07ff983a4c76a
Tian Jiang <jt2594838@163.com>
2025-02-20 16:06:02 +0800

    fix memory leak of MetdataIndexNode

--- cpp/src/reader/task/device_query_task.cc
+++ cpp/src/reader/task/device_query_task.cc
@@ -22,7 +22,7 @@
 namespace storage {
 DeviceQueryTask *DeviceQueryTask::create_device_query_task(
     std::shared_ptr<IDeviceID> device_id, std::vector<std::string> column_names,
-    std::shared_ptr<ColumnMapping> column_mapping, MetaIndexNode index_root,
+    std::shared_ptr<ColumnMapping> column_mapping, MetaIndexNode *index_root,
     std::shared_ptr<TableSchema> table_schema, common::PageArena &pa) {
     void *buf = pa.alloc(sizeof(DeviceQueryTask));
     if (UNLIKELY(buf == nullptr)) {
@@ -34,6 +34,9 @@ DeviceQueryTask *DeviceQueryTask::create_device_query_task(
 }
 
  DeviceQueryTask::~DeviceQueryTask() {
+    if (index_root_) {
+        index_root_->~MetaIndexNode();
+    }
 }
 
 }  // namespace storage
